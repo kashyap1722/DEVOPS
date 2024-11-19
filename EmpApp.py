60078179fc613fbb8e8a9f59abc343655e410a72
kashyap1722 <122462020+kashyap1722@users.noreply.github.com>
2024-11-19 14:59:42 -0500

    EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -40,41 +40,42 @@ def AddEmp():
     if emp_image_file.filename == "":
         return "Please select a file"
 
-   try:
-    insert_sql = "INSERT INTO employee (empid, firstname, lastname, priskill, location) VALUES (%s, %s, %s, %s, %s)"
-    cursor = db_conn.cursor()
-    cursor.execute(insert_sql, (empid, firstname, lastname, priskill, location))
-    db_conn.commit()
+    try:
+        insert_sql = "INSERT INTO employee (empid, firstname, lastname, priskill, location) VALUES (%s, %s, %s, %s, %s)"
+        cursor = db_conn.cursor()
+        cursor.execute(insert_sql, (empid, firstname, lastname, priskill, location))
+        db_conn.commit()
 
-    emp_name = f"{firstname} {lastname}"
+        emp_name = f"{firstname} {lastname}"
 
-    # Upload image file to S3
-    emp_image_file_name_in_s3 = f"emp-id-{empid}_image_file"
-    s3 = boto3.resource('s3')
-    print("Data inserted in MySQL RDS... uploading image to S3...")
+        # Upload image file to S3
+        emp_image_file_name_in_s3 = f"emp-id-{empid}_image_file"
+        s3 = boto3.resource('s3')
+        print("Data inserted in MySQL RDS... uploading image to S3...")
 
-    # Upload the image to the S3 bucket
-    s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
-    
-    # Get the S3 bucket location
-    bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
-    s3_location = bucket_location['LocationConstraint'] or ''
-    object_url = f"https://s3{s3_location}.amazonaws.com/{custombucket}/{emp_image_file_name_in_s3}"
+        # Upload the image to the S3 bucket
+        s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
+        
+        # Get the S3 bucket location
+        bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
+        s3_location = bucket_location['LocationConstraint'] or ''
+        object_url = f"https://s3{s3_location}.amazonaws.com/{custombucket}/{emp_image_file_name_in_s3}"
 
-    # Update the employee record with the image URL
-    update_sql = "UPDATE employee SET emp_image_url = %s WHERE empid = %s"
-    cursor.execute(update_sql, (object_url, empid))
-    db_conn.commit()
+        # Update the employee record with the image URL
+        update_sql = "UPDATE employee SET emp_image_url = %s WHERE empid = %s"
+        cursor.execute(update_sql, (object_url, empid))
+        db_conn.commit()
 
-    print("All modifications done...")
+        print("All modifications done...")
 
-except Exception as e:
-    return str(e)  # Handle exceptions by returning the error message
+    except Exception as e:
+        return str(e)  # Handle exceptions by returning the error message
 
-finally:
-    cursor.close()
+    finally:
+        cursor.close()
 
     return render_template('AddEmpOutput.html', name=emp_name, object_url=object_url)
+
 
 
 @app.route('/getemp', methods=['GET'])
